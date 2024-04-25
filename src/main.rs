f549f3548e9d28c95e66637ad46c9c9e7279502e
Hanamiyuzu <txw5ifd9@duck.com>
2024-04-25 18:00:18 +0800

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use anyhow::{Ok, Result};
-use tokio::net::TcpListener;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
     loop {
-        let (_socket, _) = listener.accept().await?;
+        let (mut socket, _) = listener.accept().await?;
+        socket.write(b"+PONG\r\n").await?;
     }
     //Ok(())
 }
