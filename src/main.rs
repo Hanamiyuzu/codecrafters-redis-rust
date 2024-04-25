bce9a67fe481a3376a4996061f193e840476c111
Hanamiyuzu <txw5ifd9@duck.com>
2024-04-25 17:51:51 +0800

    stage 1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use anyhow::{Ok, Result};
+use tokio::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    loop {
+        let (_socket, _) = listener.accept().await?;
+    }
+    //Ok(())
 }
