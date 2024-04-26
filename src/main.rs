f41fba8222337fddd3bc9374fc677051c4228bfe
HanamiYuzu <txw5ifd9@duck.com>
2024-04-26 11:51:52 +0800

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
     loop {
         let (mut socket, _) = listener.accept().await?;
-        socket.write(b"+PONG\r\n").await?;
+        socket.write(b"+PONG\r\n+PONG\r\n").await?;
     }
     //Ok(())
 }
